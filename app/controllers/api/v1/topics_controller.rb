3b67e73957a22068683700ea02bf448a103bdb98
佐藤克樹 <k-sato@ruby-dev.jp>
2018-12-20 10:37:12 +0900

    Add Destroy and update

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::TopicsController < ApplicationController
   def index
     topics = Topic.order(created_at: :desc)
-    render json: { status: 'SUCCESS', message: 'loaded posts', data: topics }, status: :ok
+    render json: { status: 'SUCCESS', message: 'loaded topics', data: topics }, status: :ok
   end
 
   def show
     topic = Topic.find(params[:id])
-    render json: { status: 'SUCCESS', message: 'loaded the post', data: topic }, status: :ok
+    render json: { status: 'SUCCESS', message: 'loaded the topic', data: topic }, status: :ok
   end
 
   def create
@@ -15,6 +15,21 @@ class Api::V1::TopicsController < ApplicationController
       render json: { status: 'SUCCESS', data: topic }, status: :ok
     else
       render json: { status: 'SUCCESS', data: topic.errors }, status: :error
+    end
+  end
+
+  def destroy
+    topic = Topic.find(params[:id])
+    topic.destroy
+    render json: { status: 'SUCCESS', message: 'deleted the topic', data: topic }, status: :ok
+  end
+
+  def update
+    topic = Topic.find(params[:id])
+    if topic.update(topic_params)
+      render json: { status: 'SUCCESS', message: 'updated the topic', data: topic }, status: :ok
+    else
+      render json: { status: 'SUCCESS', message: 'loaded the topic', data: topic }, status: :ok
     end
   end
 
